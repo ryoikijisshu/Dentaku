c63d873a9cb666b1a37921eaf446fd39149fb131
yuuka233 <yuuka199577@gmail.com>
2019-05-15 14:34:17 +0900

    ボタン設置の修正

--- app/src/main/java/kgu/klab/dentaku/MainActivity.kt
+++ app/src/main/java/kgu/klab/dentaku/MainActivity.kt
@@ -2,11 +2,36 @@ package kgu.klab.dentaku
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    var nStr : String = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        num0.setOnClickListener {
+            nStr=nStr+"0";
+        }
+        num1.setOnClickListener {
+            nStr=nStr+"1"; }
+        num2.setOnClickListener {
+            nStr=nStr+"2";
+        }
+        num3.setOnClickListener {
+            nStr=nStr+"3";  }
+        num4.setOnClickListener {
+            nStr=nStr+"4";
+        }
+        num5.setOnClickListener {
+            nStr=nStr+"5"; }
+        num6.setOnClickListener {
+            nStr=nStr+"6";}
+        num7.setOnClickListener {
+            nStr=nStr+"7"; }
+        num8.setOnClickListener {
+            nStr=nStr+"8"; }
+        num9.setOnClickListener {
+            nStr=nStr+"9"; }
+
     }
 }
