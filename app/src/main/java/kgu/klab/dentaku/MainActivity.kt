33a0a2b068edbca537d7d3217ebbb6702d6d8fe2
h_tamai <tamaomarimii@gmail.com>
2019-05-25 22:53:25 +0900

    課題1-3完了しました

--- app/src/main/java/kgu/klab/dentaku/MainActivity.kt
+++ app/src/main/java/kgu/klab/dentaku/MainActivity.kt
@@ -12,110 +12,205 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var value : Int = 0 //値を格納
+        var clear : Boolean = false //true時に数字ボタンが押された時に計算結果をクリア
+        var calc : Boolean = false //true時にのみ計算ボタンが押された時に計算処理を行う
+        var operator : String? = null //演算子を格納
+
         num0.setOnClickListener {
 
             result_text += "0"
-            result.text =result_text
+            result.text =
+                if(result.text.toString() != "0" && clear == false){
+                    result.text.toString() + "0"
+                } else {
+                clear = false
+                "0"
+                }
+            calc = true
         }
 
         num1.setOnClickListener {
 
             result_text += "1"
-            result.text =result_text
+            result.text =if(result.text.toString() != "0" && clear == false){
+                result.text.toString() + "1"
+            } else {
+                clear = false
+                "1"
+            }
 
+            calc = true
         }
 
         num2.setOnClickListener {
 
             result_text += "2"
-            result.text =result_text
+            result.text =if(result.text.toString() != "0" && clear == false){
+                result.text.toString() + "2"
+            } else {
+                clear = false
+                "2"
+            }
+            calc = true
 
         }
 
         num3.setOnClickListener {
 
             result_text += "3"
-            result.text =result_text
+            result.text =if(result.text.toString() != "0" && clear == false){
+                result.text.toString() + "3"
+            } else {
+                clear = false
+                "3"
+            }
+            calc = true
 
         }
 
         num4.setOnClickListener {
 
             result_text += "4"
-            result.text =result_text
+            result.text =if(result.text.toString() != "0" && clear == false){
+                result.text.toString() + "4"
+            } else {
+                clear = false
+                "4"
+            }
+            calc = true
 
         }
 
         num5.setOnClickListener {
 
             result_text += "5"
-            result.text =result_text
+            result.text =if(result.text.toString() != "0" && clear == false){
+                result.text.toString() + "5"
+            } else {
+                clear = false
+                "5"
+            }
+            calc = true
 
         }
 
         num6.setOnClickListener {
 
             result_text += "6"
-            result.text =result_text
+            result.text =if(result.text.toString() != "0" && clear == false){
+                result.text.toString() + "6"
+            } else {
+                clear = false
+                "6"
+            }
+            calc = true
 
         }
 
         num7.setOnClickListener {
 
             result_text += "7"
-            result.text =result_text
+            result.text =if(result.text.toString() != "0" && clear == false){
+                result.text.toString() + "7"
+            } else {
+                clear = false
+                "7"
+            }
+            calc = true
 
         }
 
         num8.setOnClickListener {
 
             result_text += "8"
-            result.text =result_text
+            result.text =if(result.text.toString() != "0" && clear == false){
+                result.text.toString() + "8"
+            } else {
+                clear = false
+                "8"
+            }
+            calc = true
 
         }
 
         num9.setOnClickListener {
 
             result_text += "9"
-            result.text =result_text
+            result.text =if(result.text.toString() != "0" && clear == false){
+                result.text.toString() + "9"
+            } else {
+                clear = false
+                "9"
+            }
+            calc = true
 
         }
 
-        func_c.setOnClickListener {
+        fun calculation(op : String?) :Int {
+            return if (op == "+") {
+                value + result.text.toString().toInt()
+            } else if (op == "-") {
+                value - result.text.toString().toInt()
+            } else if (op == "*") {
+                value * result.text.toString().toInt()
+            } else if (op == "/") {
+                value / result.text.toString().toInt()
+            } else {
+                result.text.toString().toInt()
+            }
+        }
 
-            result_text = ""
-            result.text =result_text
+        fun calcBottunAction(op : String?) {
+            if (calc == true) {
+                value = calculation(operator)
+                clear = true
+                calc = false
+                result.text = value.toString()
+            }
+            operator = op
+        }
+
+
+        func_c.setOnClickListener {
+            result.text ="0"
+            value = 0
+            operator = null
+            clear = false
+            calc = false
 
         }
 
         func_eq.setOnClickListener {
 
-            result_text += "="
-            result.text =result_text
+            if (calc == true) {
+                value = calculation(operator)
+                calc = false
+                clear = true
+                result.text = value.toString()
+                operator = null
+            }
         }
 
         func_pls.setOnClickListener {
 
-            result_text += "+"
-            result.text =result_text
+            calcBottunAction("+")
+
         }
 
         func_min.setOnClickListener {
 
-            result_text += "-"
-            result.text =result_text
+            calcBottunAction("-")
         }
 
         func_mul.setOnClickListener {
 
-            result_text += "*"
-            result.text =result_text
+            calcBottunAction("*")
         }
 
         func_div.setOnClickListener {
 
-            result_text += "/"
-            result.text =result_text
+            calcBottunAction("/")
         }
     }
 }
