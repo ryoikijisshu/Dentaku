38fb7a94b85d5e4f3b636f33b0669f7719039ca1
ryochin0114 <ryochin@kwansei.ac.jp>
2019-05-25 22:13:50 +0900

    課題1-3を修正しました

--- app/src/main/java/kgu/klab/dentaku/MainActivity.kt
+++ app/src/main/java/kgu/klab/dentaku/MainActivity.kt
@@ -10,79 +10,132 @@ class MainActivity : AppCompatActivity() {
     var num_text: Double = 0.0
     var num2_text: Double = 0.0
     var Identifier:Int = 0
+    var signal:Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        num0.setOnClickListener {
 
-            result_text += "0"
-            result.text =result_text
-        }
+                        num0.setOnClickListener {
 
-        num1.setOnClickListener {
+                            result_text += "0"
+                            if( signal == 1) {
+                                result_text = ""
+                                signal = 0
+                                result_text += "0"
+                            }
+                            result.text = result_text
+                        }
 
-            result_text += "1"
-            result.text =result_text
+                        num1.setOnClickListener {
 
-        }
+                            result_text += "1"
+                            if( signal == 1) {
+                                result_text = ""
+                                signal = 0
+                                result_text += "1"
+                            }
+                            result.text = result_text
 
-        num2.setOnClickListener {
+                        }
 
-            result_text += "2"
-            result.text =result_text
+                        num2.setOnClickListener {
 
-        }
+                            result_text += "2"
+                            if( signal == 1) {
+                                result_text = ""
+                                signal = 0
+                                result_text += "2"
+                            }
+                            result.text = result_text
 
-        num3.setOnClickListener {
+                        }
 
-            result_text += "3"
-            result.text =result_text
+                        num3.setOnClickListener {
 
-        }
+                            result_text += "3"
+                            if( signal == 1) {
+                                result_text = ""
+                                signal = 0
+                                result_text += "3"
+                            }
+                            result.text = result_text
 
-        num4.setOnClickListener {
+                        }
 
-            result_text += "4"
-            result.text =result_text
+                        num4.setOnClickListener {
 
-        }
+                            result_text += "4"
+                            if( signal == 1) {
+                                result_text = ""
+                                signal = 0
+                                result_text += "4"
+                            }
+                            result.text = result_text
 
-        num5.setOnClickListener {
+                        }
 
-            result_text += "5"
-            result.text =result_text
+                        num5.setOnClickListener {
 
-        }
+                            result_text += "5"
+                            if( signal == 1) {
+                                result_text = ""
+                                signal = 0
+                                result_text += "5"
+                            }
+                            result.text = result_text
 
-        num6.setOnClickListener {
+                        }
 
-            result_text += "6"
-            result.text =result_text
+                        num6.setOnClickListener {
 
-        }
+                            result_text += "6"
+                            if( signal == 1) {
+                                result_text = ""
+                                signal = 0
+                                result_text += "6"
+                            }
+                            result.text = result_text
 
-        num7.setOnClickListener {
+                        }
 
-            result_text += "7"
-            result.text =result_text
+                        num7.setOnClickListener {
 
-        }
+                            result_text += "7"
+                            if( signal == 1) {
+                                result_text = ""
+                                signal = 0
+                                result_text += "7"
+                            }
+                            result.text = result_text
 
-        num8.setOnClickListener {
+                        }
 
-            result_text += "8"
-            result.text =result_text
+                        num8.setOnClickListener {
 
-        }
+                            result_text += "8"
+                            if( signal == 1) {
+                                result_text = ""
+                                signal = 0
+                                result_text += "8"
+                            }
+                            result.text = result_text
 
-        num9.setOnClickListener {
+                        }
 
-            result_text += "9"
-            result.text =result_text
+                        num9.setOnClickListener {
 
-        }
+                            result_text += "9"
+                            if( signal == 1) {
+                                result_text = ""
+                                signal = 0
+                                result_text += "9"
+                            }
+                            result.text = result_text
+
+                        }
 
         func_c.setOnClickListener {
 
@@ -107,6 +160,7 @@ class MainActivity : AppCompatActivity() {
             }
             result_text = num2_text.toString()
             result.text = result_text
+            signal = 1
         }
 
         func_pls.setOnClickListener {
@@ -115,7 +169,7 @@ class MainActivity : AppCompatActivity() {
             result.text =result_text
             result_text = ""
             Identifier = 1
-
+            signal = 0
         }
 
         func_min.setOnClickListener {
@@ -124,6 +178,7 @@ class MainActivity : AppCompatActivity() {
             result.text =result_text
             result_text = ""
             Identifier = 2
+            signal = 0
         }
 
         func_mul.setOnClickListener {
@@ -132,6 +187,7 @@ class MainActivity : AppCompatActivity() {
             result.text =result_text
             result_text = ""
             Identifier = 3
+            signal = 0
         }
 
         func_div.setOnClickListener {
@@ -140,6 +196,8 @@ class MainActivity : AppCompatActivity() {
             result.text =result_text
             result_text = ""
             Identifier = 4
+            signal = 0
         }
+
     }
 }
