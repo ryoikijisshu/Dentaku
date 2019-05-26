5fa357a9f32c451f30ebde436dc7f3dca364aad6
okap0910 <fhl25209@kwansei.ac.jp>
2019-05-27 03:33:51 +0900

    電卓の作成が終わりました

--- app/src/main/java/kgu/klab/dentaku/MainActivity.kt
+++ app/src/main/java/kgu/klab/dentaku/MainActivity.kt
@@ -6,116 +6,209 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var result_text: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var a = 0.0
+        var b = 0.0
+        var c = 0
+        var result_text: String = ""
 
         num0.setOnClickListener {
 
             result_text += "0"
-            result.text =result_text
+            result.text = result_text
         }
 
         num1.setOnClickListener {
-
-            result_text += "1"
-            result.text =result_text
+            if (result_text == ""){
+                result_text = "1"
+            }
+            else{
+                result_text += "1"
+            }
+            result.text = result_text
 
         }
 
         num2.setOnClickListener {
 
-            result_text += "2"
-            result.text =result_text
+            if (result_text == ""){
+                result_text = "2"
+            }
+            else{
+                result_text += "2"
+            }
+            result.text = result_text
 
         }
 
         num3.setOnClickListener {
 
-            result_text += "3"
-            result.text =result_text
+            if (result_text == ""){
+                result_text = "3"
+            }
+            else{
+                result_text += "3"
+            }
+            result.text = result_text
 
         }
 
         num4.setOnClickListener {
 
-            result_text += "4"
-            result.text =result_text
+            if (result_text == ""){
+                result_text = "4"
+            }
+            else{
+                result_text += "4"
+            }
+            result.text = result_text
 
         }
 
         num5.setOnClickListener {
 
-            result_text += "5"
-            result.text =result_text
+            if (result_text == ""){
+                result_text = "5"
+            }
+            else{
+                result_text += "5"
+            }
+            result.text = result_text
 
         }
 
         num6.setOnClickListener {
 
-            result_text += "6"
-            result.text =result_text
+            if (result_text == ""){
+                result_text = "6"
+            }
+            else{
+                result_text += "6"
+            }
+            result.text = result_text
 
         }
 
         num7.setOnClickListener {
 
-            result_text += "7"
-            result.text =result_text
+            if (result_text == ""){
+                result_text = "7"
+            }
+            else{
+                result_text += "7"
+            }
+            result.text = result_text
 
         }
 
         num8.setOnClickListener {
 
-            result_text += "8"
-            result.text =result_text
+            if (result_text == ""){
+                result_text = "8"
+            }
+            else{
+                result_text += "8"
+            }
+            result.text = result_text
 
         }
 
         num9.setOnClickListener {
 
-            result_text += "9"
-            result.text =result_text
+            if (result_text == ""){
+                result_text = "9"
+            }
+            else{
+                result_text += "9"
+            }
+            result.text = result_text
 
         }
 
         func_c.setOnClickListener {
 
             result_text = ""
-            result.text =result_text
-
+            result.text = result_text
+            a = 0.0
+            b = 0.0
+            c = 0
         }
 
         func_eq.setOnClickListener {
-
-            result_text += "="
-            result.text =result_text
+            if(c==1){
+                b = result_text.toDouble()
+                a = a+b
+            }
+            if(c==2){
+                b = result_text.toDouble()
+                a = a-b
+            }
+            if(c==3){
+                b = result_text.toDouble()
+                a = a*b
+            }
+            if(c==4){
+                b = result_text.toDouble()
+                a = a/b
+            }
+            result.text = a.toString()
         }
 
         func_pls.setOnClickListener {
-
-            result_text += "+"
-            result.text =result_text
+            b = result_text.toDouble()
+            a = a+b
+            c = 1
+            result.text = a.toString()
+            result_text =""
         }
 
         func_min.setOnClickListener {
-
-            result_text += "-"
-            result.text =result_text
+            if(result_text == "")
+            {
+                result_text = "-"
+                result.text = result_text
+            }
+            else{
+                b = result_text.toDouble()
+                if(c==0){
+                    a=b
+                }
+                else{
+                    a = a-b
+                }
+                c = 2
+                result.text = a.toString()
+                result_text=""
+            }
         }
 
         func_mul.setOnClickListener {
-
-            result_text += "*"
-            result.text =result_text
+            b = result_text.toDouble()
+            if(c==0){
+                a=b
+            }
+            else{
+                a = a*b
+            }
+            c = 3
+            result.text = a.toString()
+            result_text=""
         }
 
         func_div.setOnClickListener {
-
-            result_text += "/"
-            result.text =result_text
+            b = result_text.toDouble()
+            if(c==0){
+                a=b
+            }
+            else{
+                a = a/b
+            }
+            c = 4
+            result.text = a.toString()
+            result_text=""
         }
     }
 }
