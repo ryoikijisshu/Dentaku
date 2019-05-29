9c1c65c9b1b0f2746473eeb6dbe58f8ee3e9fdd5
higashiguchi <dal29809@kwansei.ac.jp>
2019-05-29 17:57:48 +0900

    課題1-3

--- app/src/main/java/kgu/klab/dentaku/MainActivity.kt
+++ app/src/main/java/kgu/klab/dentaku/MainActivity.kt
@@ -7,6 +7,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     var result_text: String = ""
+    var value1 : Double = 0.0
+    var value2 : Double = 0.0
+    var value3 : Int = 0
+    var value4 : Int = 0
+    var value5 : Int = 0
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,108 +21,271 @@ class MainActivity : AppCompatActivity() {
 
         num0.setOnClickListener {
 
-            result_text += "0"
-            result.text =result_text
+            if(value4 == 0)
+            {
+                result_text += "0"
+                result.text =result_text
+            }
+            else
+            {
+                value4 = 0
+                result_text = ""
+                result_text += "0"
+                result.text =result_text
+            }
+            value5 = 1
+
         }
 
         num1.setOnClickListener {
+            if(value4 == 0)
+            {
+                result_text += "1"
+                result.text =result_text
+            }
+            else
+            {
+                value4 = 0
+                result_text = ""
+                result_text += "1"
+                result.text =result_text
+            }
+            value5 = 1
 
-            result_text += "1"
-            result.text =result_text
+
 
         }
 
         num2.setOnClickListener {
 
-            result_text += "2"
-            result.text =result_text
+            if(value4 == 0)
+            {
+                result_text += "2"
+                result.text =result_text
+            }
+            else
+            {
+                value4 = 0
+                result_text = ""
+                result_text += "2"
+                result.text =result_text
+            }
+            value5 = 1
 
         }
 
         num3.setOnClickListener {
 
-            result_text += "3"
-            result.text =result_text
-
+            if(value4 == 0)
+            {
+                result_text += "3"
+                result.text =result_text
+            }
+            else
+            {
+                value4 = 0
+                result_text = ""
+                result_text += "3"
+                result.text =result_text
+            }
+            value5 = 1
         }
 
         num4.setOnClickListener {
 
-            result_text += "4"
-            result.text =result_text
-
+            if(value4 == 0)
+            {
+                result_text += "4"
+                result.text =result_text
+            }
+            else
+            {
+                value4 = 0
+                result_text = ""
+                result_text += "4"
+                result.text =result_text
+            }
+            value5 = 1
         }
 
         num5.setOnClickListener {
 
-            result_text += "5"
-            result.text =result_text
-
+            if(value4 == 0)
+            {
+                result_text += "5"
+                result.text =result_text
+            }
+            else
+            {
+                value4 = 0
+                result_text = ""
+                result_text += "5"
+                result.text =result_text
+            }
+            value5 = 1
         }
 
         num6.setOnClickListener {
 
-            result_text += "6"
-            result.text =result_text
+            if(value4 == 0)
+            {
+                result_text += "6"
+                result.text =result_text
+            }
+            else
+            {
+                value4 = 0
+                result_text = ""
+                result_text += "6"
+                result.text =result_text
+            }
+            value5 = 1
 
         }
 
         num7.setOnClickListener {
 
-            result_text += "7"
-            result.text =result_text
-
+            if(value4 == 0)
+            {
+                result_text += "7"
+                result.text =result_text
+            }
+            else
+            {
+                value4 = 0
+                result_text = ""
+                result_text += "7"
+                result.text =result_text
+            }
+            value5 = 1
         }
 
         num8.setOnClickListener {
 
-            result_text += "8"
-            result.text =result_text
+            if(value4 == 0)
+            {
+                result_text += "8"
+                result.text =result_text
+            }
+            else
+            {
+                value4 = 0
+                result_text = ""
+                result_text += "8"
+                result.text =result_text
+            }
+            value5 = 1
 
         }
 
         num9.setOnClickListener {
 
-            result_text += "9"
-            result.text =result_text
-
+            if(value4 == 0)
+            {
+                result_text += "9"
+                result.text =result_text
+            }
+            else
+            {
+                value4 = 0
+                result_text = ""
+                result_text += "9"
+                result.text =result_text
+            }
+            value5 = 1
         }
 
         func_c.setOnClickListener {
 
             result_text = ""
             result.text =result_text
+            value5 = 0
 
         }
 
         func_eq.setOnClickListener {
+            if(value5 == 0) {
+            }
+            else {
 
-            result_text += "="
-            result.text =result_text
+
+                value2 = result_text.toDouble()
+                if (value3 == 1) {
+                    value2 = value1 + value2
+                }
+                if (value3 == 2) {
+                    value2 = value1 - value2
+                }
+                if (value3 == 3) {
+                    value2 = value1 * value2
+                }
+                if (value3 == 4) {
+                    value2 = value1 / value2
+                }
+
+                value4 = 1
+
+                result_text = value2.toString()
+                result.text = result_text
+            }
+
+
         }
 
         func_pls.setOnClickListener {
+            if(value5 == 0) {
+            }
+            else {
+                value1 = result_text.toDouble()
+                result_text += "+"
+                result.text = result_text
+                result_text = ""
+                value3 = 1
+                value5 = 0
+            }
 
-            result_text += "+"
-            result.text =result_text
         }
 
         func_min.setOnClickListener {
 
-            result_text += "-"
-            result.text =result_text
+            if(value5 == 0) {
+            }
+            else {
+                value1 = result_text.toDouble()
+                result_text += "-"
+                result.text = result_text
+                result_text = ""
+                value3 = 2
+                value5 = 0
+            }
         }
 
         func_mul.setOnClickListener {
 
-            result_text += "*"
-            result.text =result_text
+            if(value5 == 0) {
+            }
+            else {
+                value1 = result_text.toDouble()
+                result_text += "*"
+                result.text = result_text
+                result_text = ""
+                value3 = 3
+                value5 = 0
+            }
         }
 
         func_div.setOnClickListener {
 
-            result_text += "/"
-            result.text =result_text
+            if(value5 == 0) {
+            }
+            else {
+                value1 = result_text.toDouble()
+                result_text += "/"
+                result.text = result_text
+                result_text = ""
+                value3 = 4
+                value5 = 0
+            }
         }
     }
 }
